857566a5588dd942e72387dec0cfd32f100c2296
Alice Novell <anovellmazzara@gmail.com>
2021-03-11 01:30:28 +0100

    v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,21 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: `YOUR_READONLY_API_TOKEN`,
+  
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+  
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+      },
+    },
   ],
 }
